da2a6a206c282f23c1387295035d0b152e31ec16
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2022-04-02 19:24:08 +0300

    P1 - We gots iterop

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,13 @@
-fun main() {
-    println("Hello, Kotlin/Native!")
+import kotlinx.cinterop.*
+import libcurl.*
+
+fun main(args: Array<String>) {
+    curl_easy_init()?.apply {
+        curl_easy_setopt(this, CURLOPT_URL, "https://example.com")
+        curl_easy_setopt(this, CURLOPT_FOLLOWLOCATION, 1L)
+        val res = curl_easy_perform(this)
+        if (res != CURLE_OK)
+            println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
+        curl_easy_cleanup(this)
+    }
 }
