9af1faef251b5682df8a72d20f347f8424a444ff
Simo Andreev <simeon.zlatanov.andreev@gmail.com>
2022-04-02 22:16:33 +0300

    P3 - glfw init

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,15 +1,20 @@
-import kotlinx.cinterop.*
-import libcurl.*
-import platform.*
-import liba.*
+import libglew.GLboolean
+import libglfw.GLFW_TRUE
+import libglfw.glfwInit
+import libglfw.glfwTerminate
+import platform.posix.exit
 
-fun main(args: Array<String>) {
-    curl_easy_init()?.apply {
-        curl_easy_setopt(this, CURLOPT_URL, "https://example.com")
-        curl_easy_setopt(this, CURLOPT_FOLLOWLOCATION, 1L)
-        val res = curl_easy_perform(this)
-        if (res != CURLE_OK)
-            println("curl_easy_perform() failed ${curl_easy_strerror(res)?.toKString()}")
-        curl_easy_cleanup(this)
+const val GL_TRUE: GLboolean = 1u
+const val GL_FALSE: GLboolean = 0u
+
+fun main() {
+    val glfwInitResul = glfwInit()
+    if (glfwInitResul != GLFW_TRUE) {
+        println("Failed to initialize GLFW!")
+        exit(glfwInitResul)
     }
+
+
+    glfwTerminate()
 }
+
